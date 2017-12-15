ca28f3e1ae796fc98d808592cc8925cb875e68da
hering <heringlinux@yeah.net>
2017-12-15 16:30:15 +0800

    Modify the format

--- main.cpp
+++ main.cpp
@@ -1,51 +1,51 @@
 #include "Window.h"
 
-const char* fmt[2] =
-{
-	"find %s -type f %s|xargs grep -nE '([^[:alnum:]_]%s[^[:alnum:]_]|^%s$|[^[:alnum:]_]%s$|^%s[^[:alnum:]_])'",
-	"find %s -type f %s|xargs grep -n %s"
+const char* fmt[2] = {
+    "find %s -type f %s|xargs grep -nE '([^[:alnum:]_]%s[^[:alnum:]_]|^%s$|[^[:alnum:]_]%s$|^%s[^[:alnum:]_])'",
+    "find %s -type f %s|xargs grep -n %s"
 };
 
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
-	string target;
-	string path = "";
-	int enhence = 0;
-	if (!get_arg(argc, argv, target, path, enhence))
+    string target;
+    string path = "";
+    int enhence = 0;
+    if (!get_arg(argc, argv, target, path, enhence))
 	{
-		return -1;
-	}
-	if (!get_filter())
+        return -1;
+    }
+    if (!get_filter())
 	{
-		return -1;
-	}
-	
-	string options = "";
-	combining(options);
+        return -1;
+    }
 
-	char search_cmd[1024];
-	if (enhence == 0)
-		snprintf(search_cmd, sizeof(search_cmd), fmt[enhence], path.c_str(), options.c_str(),
-			target.c_str(), target.c_str(), target.c_str(), target.c_str());
-	else
-		snprintf(search_cmd, sizeof(search_cmd), fmt[enhence], path.c_str(), options.c_str(), target.c_str());
-	
-	vector<string> vec_res;
-	if (!run_cmd(vec_res, search_cmd))
+    string options = "";
+    combining(options);
+
+    char search_cmd[1024];
+    if (enhence == 0)
+        snprintf(search_cmd, sizeof(search_cmd), fmt[enhence], path.c_str(), options.c_str(),
+            target.c_str(), target.c_str(), target.c_str(), target.c_str());
+    else
+        snprintf(search_cmd, sizeof(search_cmd), fmt[enhence], path.c_str(), options.c_str(), target.c_str());
+
+    vector<string> vec_res;
+    if (!run_cmd(vec_res, search_cmd))
 	{
-		return -1;
-	}
-	if (vec_res.size() > 0)
+        return -1;
+    }
+    if (vec_res.size() > 0)
 	{
-		CreateMenu(vec_res);
-		int index = Waiting(vec_res);
-		CloseMenu();
-		if (index != -1)
+        CreateMenu(vec_res);
+        int index = Waiting(vec_res);
+        CloseMenu();
+        if (index != -1)
 		{
-			pair<string,string> pair = get_file_lines(vec_res[index]);
-			system(string("vim " + pair.first + " +" + pair.second).c_str());;
-		}
-	}
-	
-	return 0;
+            pair<string, string> pair = get_file_lines(vec_res[index]);
+            system(string("vim " + pair.first + " +" + pair.second).c_str());
+            ;
+        }
+    }
+
+    return 0;
 }
--- utils.cpp
+++ utils.cpp
@@ -6,29 +6,29 @@ map<string, string> map_filter;
 
 string strip(string str, bool del_left = true)
 {
-	if (str.length() > 0)
-	{
-		while (del_left && 
-			(*str.begin() == ' ' || *str.begin() == '\n' || *str.begin() == '\r' || *str.rbegin() == '\t'))
-		{
-			str.erase(0, 1);
-		}
-		while (*str.rbegin() == ' ' || *str.rbegin() == '\n' || *str.rbegin() == '\r' || *str.rbegin() == '\t')
-		{
-			str.erase(str.length() - 1);
-		}
-	}
+    if (str.length() > 0)
+    {
+        while (del_left && 
+            (*str.begin() == ' ' || *str.begin() == '\n' || *str.begin() == '\r' || *str.rbegin() == '\t'))
+        {
+            str.erase(0, 1);
+        }
+        while (*str.rbegin() == ' ' || *str.rbegin() == '\n' || *str.rbegin() == '\r' || *str.rbegin() == '\t')
+        {
+            str.erase(str.length() - 1);
+        }
+    }
     return str;
 }
 
 bool get_filter()
 {
     map_filter.clear();
-	if (access(filter_file, F_OK) != 0)
-	{
-		cout << "The profile does not exist." << endl;
-		return false;
-	}
+    if (access(filter_file, F_OK) != 0)
+    {
+        cout << "The profile does not exist." << endl;
+        return false;
+    }
     FILE* fp = fopen(filter_file, "r");
     if (fp)
     {
@@ -83,7 +83,7 @@ bool combining(string& options)
         {
             options += " -a ";
         }
-		auto pos = string::npos;
+        auto pos = string::npos;
         options += "\\( ";
         auto str = map_filter[NAME_IG];
         while (str.length() > 0)
@@ -108,7 +108,7 @@ bool combining(string& options)
         {
             options += " -a ";
         }
-		auto pos = string::npos;
+        auto pos = string::npos;
         options += "\\( ";
         auto str = map_filter[DIR_IG];
         while (str.length() > 0)
@@ -133,7 +133,7 @@ bool combining(string& options)
         {
             options += " -a ";
         }
-		auto pos = string::npos;
+        auto pos = string::npos;
         options += "\\(";
         auto str = map_filter[PERM];
         while (str.length() > 0)
@@ -158,117 +158,117 @@ bool combining(string& options)
 
 bool get_arg(int& argc, char** argv, string& target, string& path, int& enhence)
 {
-	if (argc == 1 || argc > 4)
-	{
-		usage();
-		return false;
-	}
-	
-	switch (argc)
-	{
-	case 2:
-		target = argv[1];
-		break;
-	case 3:
-		if (string(argv[1]) == "-E")
-		{
-			enhence = 1;
-			target = argv[2];
-		}
-		else if (string(argv[2]) == "-E")
-		{
-			enhence = 1;
-			target = argv[1];
-		}
-		else
-		{
-			path = argv[1];
-			if (access(path.c_str(), F_OK) != 0)
-			{
-				cout << "The path '" + path + "' does not exist." << endl;
-				return false;
-			}
-			target = argv[2];
-		}
-		break;
-	case 4:
-		if (string(argv[1]) == "-E")
-		{
-			enhence = 1;
-			path = argv[2];
-			target = argv[3];
-		}
-		else if (string(argv[2]) == "-E")
-		{
-			enhence = 1;
-			path = argv[1];
-			target = argv[3];
-		}
-		else if (string(argv[3]) == "-E")
-		{
-			enhence = 1;
-			path = argv[1];
-			path = argv[2];
-		}
-		else
-		{
-			usage();
-			return false;
-		}
-		if (access(path.c_str(), F_OK) != 0)
-		{
-			cout << "The path '" + path + "' does not exist." << endl;
-			return false;
-		}
-		break;
-	default:
-		usage();
-		return false;
-	}
-	return true;
+    if (argc == 1 || argc > 4)
+    {
+        usage();
+        return false;
+    }
+    
+    switch (argc)
+    {
+    case 2:
+        target = argv[1];
+        break;
+    case 3:
+        if (string(argv[1]) == "-E")
+        {
+            enhence = 1;
+            target = argv[2];
+        }
+        else if (string(argv[2]) == "-E")
+        {
+            enhence = 1;
+            target = argv[1];
+        }
+        else
+        {
+            path = argv[1];
+            if (access(path.c_str(), F_OK) != 0)
+            {
+                cout << "The path '" + path + "' does not exist." << endl;
+                return false;
+            }
+            target = argv[2];
+        }
+        break;
+    case 4:
+        if (string(argv[1]) == "-E")
+        {
+            enhence = 1;
+            path = argv[2];
+            target = argv[3];
+        }
+        else if (string(argv[2]) == "-E")
+        {
+            enhence = 1;
+            path = argv[1];
+            target = argv[3];
+        }
+        else if (string(argv[3]) == "-E")
+        {
+            enhence = 1;
+            path = argv[1];
+            path = argv[2];
+        }
+        else
+        {
+            usage();
+            return false;
+        }
+        if (access(path.c_str(), F_OK) != 0)
+        {
+            cout << "The path '" + path + "' does not exist." << endl;
+            return false;
+        }
+        break;
+    default:
+        usage();
+        return false;
+    }
+    return true;
 }
 
 bool run_cmd(vector<string>& vec_res, char* cmd)
 {
-	FILE* fp = popen(cmd, "r");
-	if (fp)
-	{
-		char res_buff[1024];
-		while (fgets(res_buff, sizeof(res_buff), fp))
-		{
-			auto str = strip(res_buff, false);
-			vec_res.push_back(deltab(str));
-		}
-		pclose(fp);
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+    FILE* fp = popen(cmd, "r");
+    if (fp)
+    {
+        char res_buff[1024];
+        while (fgets(res_buff, sizeof(res_buff), fp))
+        {
+            auto str = strip(res_buff, false);
+            vec_res.push_back(deltab(str));
+        }
+        pclose(fp);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 void usage()
 {
-	cout << "Usage: search [path] [-E] <target>" << endl;
-	cout << "   -E: Enhancement mode, it may be in command anywhere." << endl;
+    cout << "Usage: search [path] [-E] <target>" << endl;
+    cout << "   -E: Enhancement mode, it may be in command anywhere." << endl;
 }
 
 pair<file_name, file_lines> get_file_lines(string str)
 {
-	auto pos = str.find(':');
+    auto pos = str.find(':');
     auto name = str.substr(0, pos);
     auto lines = str.substr(pos + 1, str.find(':', pos + 1) - pos -1);
-	return make_pair(name, lines);
+    return make_pair(name, lines);
 }
 
 string& deltab(string& str)
 {
-	auto pos = string::npos;
-	while ((pos = str.find('\t')) != string::npos)
-	{
-		str.erase(pos, 1);
-		str.insert(pos, 4, ' ');
-	}
-	return str;
+    auto pos = string::npos;
+    while ((pos = str.find('\t')) != string::npos)
+    {
+        str.erase(pos, 1);
+        str.insert(pos, 4, ' ');
+    }
+    return str;
 }
